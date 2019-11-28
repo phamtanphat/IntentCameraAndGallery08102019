c3248961a457e86623be4982792f8eb03f9bb110
phatdroid94 <phatdroid94@gmail.com>
2019-11-28 21:27:45 +0700

    request permission camera

--- app/src/main/java/com/example/intentcameraandgallery08102019/MainActivity.java
+++ app/src/main/java/com/example/intentcameraandgallery08102019/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.intentcameraandgallery08102019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCamera,btnGallery;
+    ImageView img;
+    int REQUEST_CODE_CAMERA = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCamera = findViewById(R.id.buttonCamera);
+        btnGallery = findViewById(R.id.buttonGallery);
+        img = findViewById(R.id.imageview);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        REQUEST_CODE_CAMERA
+                );
+            }
+        });
     }
 }
